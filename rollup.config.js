b2157fc74a3b4eea0b8151d8cbd30b0b5efed199
xingqingxyz <cm.email@qq.com>
2024-09-19 00:02:47 +0800

    fix(rollup): corrects extension packging

--- rollup.config.js
+++ rollup.config.js
@@ -3,30 +3,28 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
-import wasm from '@rollup/plugin-wasm'
 import { defineConfig } from 'rollup'
 
-const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 
 export default defineConfig({
   input: 'src/extension.ts',
   output: {
     file: 'out/extension.js',
     format: 'cjs',
-    sourcemap: isProd,
+    sourcemap: isDev,
   },
   external: ['vscode', '@johnnymorganz/stylua'],
   plugins: [
-    wasm(),
     typescript(),
     nodeResolve(),
     commonjs(),
     replace({
       preventAssignment: true,
       values: {
-        __DEV__: !isProd,
+        __DEV__: isDev,
       },
     }),
-    isProd && terser(),
+    !isDev && terser(),
   ],
 })
